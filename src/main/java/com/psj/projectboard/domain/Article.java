ff5e8d45f5116b98e5a2997789d31c38dc9ce9da
SangJin <asdfg2977@naver.com>
2023-03-16 19:15:15 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관이 없는 요소이므로 추출이 가능하다.
    '@MappedSuperclass' 이용하여 상속 방식으로 추출함
    
    그외 '@DateTimeFormat' 요소 추가 및 jpa 옵션 개선

--- src/main/java/com/psj/projectboard/domain/Article.java
+++ src/main/java/com/psj/projectboard/domain/Article.java
@@ -23,9 +23,9 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class Article {
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,11 +42,11 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 양방향 바인딩
 
-    // 메타 데이터 , 자동 세팅 가능하도록 설정
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+    // 메타 데이터 , 자동 세팅 가능하도록 설정, AuditingFields로 분리
+//    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+//    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
+//    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+//    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
     // 평소에는 오픈하지 않기 때문에 public을 사용하지 않는다.
     protected Article(){}
