f33d52047df9d3890872c65a637768d7f716b397
SangJin <asdfg2977@naver.com>
2023-05-30 23:02:39 +0900

    #30 - 검색 기능을 위해 SearchType 옵션 전달, 테스트 업데이트
    게시판 페이지의 검색바에서 검색 종류를 드롭 다운 메뉴로 제공할 때, 해당 값을 뷰가 알게하지 않고 서버로부터 받아서 쓰게 만들기 위해 값을 전달하는 로직을 테스트와 함께 추가

--- src/main/java/com/psj/projectboard/controller/ArticleController.java
+++ src/main/java/com/psj/projectboard/controller/ArticleController.java
@@ -37,8 +37,10 @@ public class ArticleController {
         Page<ArticleResponse> articles = articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from); // Page인터페이스 안에 정렬 기능이 존재한다.
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
+
         map.addAttribute("articles", articles); // searchArticles가 dto를 보내주기 때문에 mapper로 가공해준다.
         map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchTypes", SearchType.values());  // enum 의 메서드
         return "articles/index";
     }
 
@@ -48,13 +50,10 @@ public class ArticleController {
         map.addAttribute("article",article );
         map.addAttribute("articleComments", article.articleCommentsResponse());
         return "articles/detail";
-
 /*  1. @GetMapping path의 {변수} 괄호안에 괄호 그레이스를 열어 URI에 사용될 변수 명을 입력한다.
 
        GetMapping 뿐만 아니라 당연히 다른 Method도 가능하다.  ( PostMapping, DeleteMapping, PutMapping......)
 
     2. @PathVariable 어노테이션을 이용해서 {템플릿 변수} 와 동일한 이름을 갖는 파라미터를 추가하면 된다. */
     }
-
-
 }
